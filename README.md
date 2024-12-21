de8e2e79ae979005974c6c7d8b53f4b3bd2d9faf
shuyshuys <ivlyns@protonmail.com>
2024-12-21 16:28:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Sistem PemilihanProject Management Terbaik dengan Weighted Product
+
+## Instalasi
+
+Silakan periksa panduan instalasi laravel resmi untuk mengetahui persyaratan server sebelum Anda mulai. [Official Documentation](https://laravel.com/docs/10.x/installation)
+
+Clone repositori
+
+```
+git clone https://github.com/varreltantio/spk-weighted-product.git
+```
+
+Beralih ke folder repo
+
+```
+cd spk-weighted-product
+```
+
+Instal semua dependensi menggunakan composer
+
+```
+composer install
+```
+
+Salin contoh berkas env dan buat perubahan konfigurasi yang diperlukan dalam berkas .env
+
+```
+cp .env.example .env
+```
+
+Buat application key baru
+
+```
+php artisan key:generate
+```
+
+Import database yang ada di [database/weighted_product.sql](https://github.com/varreltantio/sistem-pemilihan-karyawan-terbaik-dengan-weighted-product/blob/main/database/weighted_product.sql) ke DBMS
+
+Mulai local development server
+
+```
+php artisan serve
+```
+
+Akses aplikasi di http://localhost:8000
+
+## Daftar Akun
+
+### Akun Admin
+
+email = admin@gmail.com
+
+password = 12345
+
+### Akun Karyawan
+
+email = deni@gmail.com
+
+password = 12345
---
+++ resources/views/assessments/bestEmployee.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.main')
+
+@section('title', 'DaftarProject Management Terbaik')
+@section('content')
+    <div class="acc-header col-xl-3 col-md-6 py-2 mt-4 rounded-3 d-flex justify-content-center">Project Management Terbaik
+    </div>
+
+    <div class="row my-4">
+        <div class="col-xl-4 col-md-6">
+            <a href="{{ route('assessments.index') }}"><button type="button" class="btn back-btn me-3">
+                    <i class="fa-solid fa-arrow-left"></i>
+                    Kembali
+                </button>
+            </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="tabel-adm col-xl-3 col-md-6 mx-auto">
+            <h4 class="pt-4 pb-3">Hasil Perhitungan Project Management Terbaik</h4>
+            <div class="table-responsive w-auto">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Nama Project Management</th>
+                            <th>Perhitungan Si</th>
+                            <th>Perhitungan Vi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $maxVi = max(array_column($resultsVi, 'vi'));
+                        @endphp
+
+                        @foreach ($resultsSi as $resultSi)
+                            @php
+                                $resultVi = collect($resultsVi)
+                                    ->where('user_uuid', $resultSi['user_uuid'])
+                                    ->first();
+                                $isMaxVi = $resultVi['vi'] == $maxVi;
+                            @endphp
+
+                            <tr class="{{ $isMaxVi ? 'table-primary' : '' }}">
+                                <td>{{ $resultVi['name'] }}</td>
+                                <td>{{ $resultSi['si'] }}</td>
+                                <td>{{ $resultVi['vi'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
